25783153dbe3460a1fa8903ecc1f15f7c688b534
starlying <corp@siteserver.cn>
2016-04-27 12:23:02 +0800

    fix

--- src/fslib/jsx/index.ts
+++ src/fslib/jsx/index.ts
@@ -3,175 +3,21 @@ var jsdom = require('jsdom-no-contextify').jsdom
 const HTMLtoJSX = require('htmltojsx')
 var defaultView = jsdom().defaultView;
 
-import * as models from '../../models'
-
-function createElement(tag) {
-  return defaultView.document.createElement(tag);
-}
-
-function isEmpty(string) {
-   return !/[^\s]/.test(string)
-}
-
-var ATTRIBUTE_MAPPING = {
-  'classname': 'className',
-  'activestyle': 'activeStyle',
-  'htmlfor': 'htmlFor'
-}
-
-class TagToReactRouter {
-  output: string
-  routerLink: boolean
-
-  reset() {
-    this.output = ''
-    this.routerLink = false
-  }
-
-  oneLevelOnly(element) {
-
-    //based on https://github.com/reactjs/react-magic/blob/master/src/htmltojsx.js#L253
-    // Only a single child element
-    if (
-      element.childNodes.length === 1
-      && element.childNodes[0].nodeType === 1
-    ) {
-      return true
-    }
-    // Only one element, and all other children are whitespace
-    var foundElement = false
-    for (var i = 0, count = element.childNodes.length; i < count; i++) {
-      var child = element.childNodes[i]
-      if (child.nodeType === 1) {
-        if (foundElement) {
-          // Encountered an element after already encountering another one
-          // Therefore, more than one element at root level
-          return false
-        } else {
-          foundElement = true
-        }
-      } else if (child.nodeType === 3 && !isEmpty(child.textContent)) {
-        // Contains text content
-        return false
-      }
-    }
-    return true
-  }
-
-  parse(element) {
-    this.reset()
-
-    var wrapper = createElement('div')
-    wrapper.innerHTML = element
-
-    if (this.oneLevelOnly(wrapper)) {
-      this.traverse(wrapper)
-    } else {
-      this.visit(wrapper)
-    }
-
-    return this.output
-  }
-
-  traverse(element) {
-    for (var i = 0, count = element.childNodes.length; i < count; i++) {
-      this.visit(element.childNodes[i])
-    }
-  }
-
-  visit(element) {
-    this.begin(element)
-    this.traverse(element)
-    this.end(element)
-  }
-
-  handleText(element) {
-    var text = element.textContent
-    if (this.routerLink) {
-      text = text.replace(/\[/g, '{').replace(/]/g, '}')
-    }
-
-    var tempEl = createElement('div')
-    tempEl.textContent = text
-
-    this.output += tempEl.innerHTML
-  }
-
-  beginNode(node) {
-    var tagName = this.routerLink ? 'Link' : node.tagName.toLowerCase()
-    var attributes = []
-    for (var i = 0, count = node.attributes.length; i < count; i++) {
-      var value = node.attributes[i].value
-      if (value.indexOf('[') === -1) {
-        value = '"' + value + '"'
-      } else {
-        value = '{' + value.replace(/\[/g, '{').replace(/\]/g, '}') + '}'
-      }
-
-      var name = node.attributes[i].name
-      if (this.routerLink) {
-        name = name.replace(/data-/g, '')
-      }
-
-      for (var key in ATTRIBUTE_MAPPING) {
-        if (ATTRIBUTE_MAPPING.hasOwnProperty(key)) {
-          name = name.replace(key, ATTRIBUTE_MAPPING[key])
-        }
-      }
-      attributes.push(name + '=' + value)
-    }
-
-    this.output += '<' + tagName
-    if (attributes.length > 0) {
-      this.output += ' ' + attributes.join(' ')
-    }
-    if (node.firstChild) {
-      this.output += '>'
-    }
-  }
-
-  begin(node) {
-    switch (node.nodeType) {
-      case 1:
-        if (node.tagName === 'A' && node.getAttribute('data-to')) {
-          this.routerLink = true
-        }
-        this.beginNode(node)
-        break
-
-      case 3:
-        this.handleText(node)
-        break
-    }
-  }
-
-  end(node) {
-    if (node.nodeType === 1) {
-      var tagName = this.routerLink ? 'Link' : node.tagName.toLowerCase()
-      this.routerLink = false
-      if (node.firstChild) {
-        this.output += '</' + tagName + '>'
-      } else {
-        this.output += ' />'
-      }
-    }
-  }
-}
+import parser from './parser'
 
 export function createReactComponent(content) {
-  var converter = new HTMLtoJSX({
+  const htmlToJSX = new HTMLtoJSX({
     createClass: false
   })
 
   var output = `const React = require('react');
-  const page = React.createClass({
+  module.exports = React.createClass({
     render: function() {
       return (
-        ` + converter.convert(new TagToReactRouter().parse(content)) + `
+        ` + htmlToJSX.convert(parser.parse(content)) + `
       )
     }
-  });
-  module.exports = page;`
+  });`
 
   return output
 }
---
+++ src/fslib/jsx/parser.js
@@ -0,0 +1,135 @@
+"use strict";
+var jsdom = require('jsdom-no-contextify').jsdom;
+var defaultView = jsdom().defaultView;
+function createElement(tag) {
+    return defaultView.document.createElement(tag);
+}
+function isEmpty(string) {
+    return !/[^\s]/.test(string);
+}
+var ATTRIBUTE_MAPPING = {
+    'classname': 'className',
+    'activestyle': 'activeStyle',
+    'htmlfor': 'htmlFor'
+};
+var Parser = (function () {
+    function Parser() {
+    }
+    Parser.prototype.reset = function () {
+        this.output = '';
+        this.routerLink = false;
+    };
+    Parser.prototype.oneLevelOnly = function (element) {
+        if (element.childNodes.length === 1
+            && element.childNodes[0].nodeType === 1) {
+            return true;
+        }
+        var foundElement = false;
+        for (var i = 0, count = element.childNodes.length; i < count; i++) {
+            var child = element.childNodes[i];
+            if (child.nodeType === 1) {
+                if (foundElement) {
+                    return false;
+                }
+                else {
+                    foundElement = true;
+                }
+            }
+            else if (child.nodeType === 3 && !isEmpty(child.textContent)) {
+                return false;
+            }
+        }
+        return true;
+    };
+    Parser.prototype.parse = function (element) {
+        this.reset();
+        var wrapper = createElement('div');
+        wrapper.innerHTML = element;
+        if (this.oneLevelOnly(wrapper)) {
+            this.traverse(wrapper);
+        }
+        else {
+            this.visit(wrapper);
+        }
+        return this.output;
+    };
+    Parser.prototype.traverse = function (element) {
+        for (var i = 0, count = element.childNodes.length; i < count; i++) {
+            this.visit(element.childNodes[i]);
+        }
+    };
+    Parser.prototype.visit = function (element) {
+        this.begin(element);
+        this.traverse(element);
+        this.end(element);
+    };
+    Parser.prototype.handleText = function (element) {
+        var text = element.textContent;
+        if (this.routerLink) {
+            text = text.replace(/\[/g, '{').replace(/]/g, '}');
+        }
+        var tempEl = createElement('div');
+        tempEl.textContent = text;
+        this.output += tempEl.innerHTML;
+    };
+    Parser.prototype.beginNode = function (node) {
+        var tagName = this.routerLink ? 'Link' : node.tagName.toLowerCase();
+        var attributes = [];
+        for (var i = 0, count = node.attributes.length; i < count; i++) {
+            var value = node.attributes[i].value;
+            if (value.indexOf('[') === -1) {
+                value = '"' + value + '"';
+            }
+            else {
+                value = '{' + value.replace(/\[/g, '{').replace(/\]/g, '}') + '}';
+            }
+            var name = node.attributes[i].name;
+            if (this.routerLink) {
+                name = name.replace(/data-/g, '');
+            }
+            for (var key in ATTRIBUTE_MAPPING) {
+                if (ATTRIBUTE_MAPPING.hasOwnProperty(key)) {
+                    name = name.replace(key, ATTRIBUTE_MAPPING[key]);
+                }
+            }
+            attributes.push(name + '=' + value);
+        }
+        this.output += '<' + tagName;
+        if (attributes.length > 0) {
+            this.output += ' ' + attributes.join(' ');
+        }
+        if (node.firstChild) {
+            this.output += '>';
+        }
+    };
+    Parser.prototype.begin = function (node) {
+        switch (node.nodeType) {
+            case 1:
+                if (node.tagName === 'A' && node.getAttribute('data-to')) {
+                    this.routerLink = true;
+                }
+                this.beginNode(node);
+                break;
+            case 3:
+                this.handleText(node);
+                break;
+        }
+    };
+    Parser.prototype.end = function (node) {
+        if (node.nodeType === 1) {
+            var tagName = this.routerLink ? 'Link' : node.tagName.toLowerCase();
+            this.routerLink = false;
+            if (node.firstChild) {
+                this.output += '</' + tagName + '>';
+            }
+            else {
+                this.output += ' />';
+            }
+        }
+    };
+    return Parser;
+}());
+var parser = new Parser();
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = parser;
+//# sourceMappingURL=parser.js.map
